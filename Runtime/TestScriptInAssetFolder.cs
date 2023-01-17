b57f896e5de5ae4c9912fcb7476205fda2088dba
Rex <s46129@yahoo.com.tw>
2023-01-18 02:39:05 +0800

    test : update script

--- Runtime/TestScriptInAssetFolder.cs
+++ Runtime/TestScriptInAssetFolder.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class TestScriptInAssetFolder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    ///測試更新
 }
